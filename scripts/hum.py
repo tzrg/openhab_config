950c5db295c120a1e48b4bf9081e5d7a4d102341
Tim Gronarz <timgronarz@posteo.de>
2024-09-29 23:23:17 +0200

    add openhab config init

---
+++ scripts/hum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2.7
+import sys
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT11
+
+pin = 2
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+if humidity is not None and temperature is not None:
+
+     print('{0:0.1f}'.format(humidity))
+
+else:
+
+     print('Failed to get reading. Try again!')
+
+sys.exit(1)
+
